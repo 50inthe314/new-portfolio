9973ef05320a81d0f8f3e8e21db6c72c7c195165
Matt Fitzpatrick <matt@50inthe314.com>
2018-10-16 08:48:27 -0400

    updated plugin `Elementor` version 2.2.5

--- wp-content/plugins/elementor/assets/js/editor.js
+++ wp-content/plugins/elementor/assets/js/editor.js
@@ -1,4 +1,4 @@
-/*! elementor - v2.2.4 - 20-09-2018 */
+/*! elementor - v2.2.5 - 07-10-2018 */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
@@ -17066,6 +17066,10 @@ PanelElementsLayoutView = Marionette.LayoutView.extend({
 
 		// TODO: Change the array from server syntax, and no need each loop for initialize
 		_.each(elementor.config.widgets, function (widget) {
+			if (elementor.config.document.panel.widgets_settings[widget.widget_type]) {
+				widget = _.extend(widget, elementor.config.document.panel.widgets_settings[widget.widget_type]);
+			}
+
 			if (!widget.show_in_panel) {
 				return;
 			}
